8e78489b09f1b556926b04f0a78e218536bf5644
Johannes Riecken <johannes.riecken@gmail.com>
2020-07-22 14:03:24 +0200

    Add const modifiers

--- distributed_tracing.cpp
+++ distributed_tracing.cpp
@@ -1,5 +1,19 @@
+// Running the program: Create file graph.csv with one comma-separated line
+// specifying a graph (without spaces). Compile this file with `g++ -std=c++17
+// distributed_tracing.cpp` (I would also gladly provide instructions for
+// compilation using Visual Studio). After compilation the program can be run by
+// running the generated ./a.out file. The unit tests are commented out to
+// simplify compilation, which would otherwise require the gtest library and the
+// addition of `-lgtest` to the end of the g++ command-line parameters. If I had
+// more time, I would add:
+// - Error handling, especially for non-existing file and argument validation
+// - Documentation comments
+// - Improve the complexity of some algorithms (I'm aware of Dijkstra's
+// algorithm)
+// - Minor amendments like giving more variables `const` and `auto` qualifiers
 #include <algorithm>
 #include <array>
+#include <fstream>
 #include <iostream>
 #include <iterator>
 #include <map>
@@ -7,51 +21,57 @@
 #include <optional>
 #include <sstream>
 #include <string>
+#include <tuple>
 #include <vector>
 
-#include "gtest/gtest.h"
+/* #include "gtest/gtest.h" */
 
 using namespace std;
 
 class Graph {
 public:
-  static constexpr array<char, 5> NODES{{'A', 'B', 'C', 'D', 'E'}};
+  static constexpr array<const char, 5> NODES{{'A', 'B', 'C', 'D', 'E'}};
   map<string, int> graph{};
 
-  explicit Graph(const string &edges_str) {
+  explicit Graph(string &edges_str) {
     vector<string> edges{};
-    istringstream ss{edges_str};
     string token;
-    while (getline(ss, token, ',')) {
-      edges.push_back(token);
-    }
-    for (const string &edge : edges) {
-      graph[edge.substr(0, 2)] = stoi(edge.substr(2, 1));
-    }
+    replace(edges_str.begin(), edges_str.end(), ',', ' ');
+    istringstream ss{edges_str};
+    istream_iterator<string> it{ss};
+    copy(it, istream_iterator<string>{}, back_inserter(edges));
+    transform(edges.begin(), edges.end(), inserter(graph, graph.begin()), [](const auto &e) {
+      return make_pair<string, int>(e.substr(0, 2), stoi(e.substr(2)));
+    });
   }
 
-  optional<int> average_latency(const vector<char> &trace) {
+  optional<int> average_latency(const vector<char> &trace) const {
     int latency = 0;
     vector<string> edges{};
     transform(trace.begin(), trace.end() - 1, trace.begin() + 1,
               back_inserter(edges), [](char a, char b) { return ""s + a + b; });
-    for (const string &edge : edges) {
-      if (graph.find(edge) == graph.end()) {
-        return nullopt;
-      }
-      latency += graph[edge];
-    }
-    return optional<int>{latency};
+    auto f = find_if(edges.begin(), edges.end(), [&](const auto& edge){if (graph.find(edge) == graph.end()) {
+            return true;
+            }
+            latency += (*graph.find(edge)).second;
+            return false;
+            });
+    return f == edges.end() ? optional<int>(latency) : nullopt;
   }
 
-  vector<vector<char>> traces(char start_node, char end_node, int min_hops,
-                              int max_hops,
-                              int max_latency = numeric_limits<int>().max()) {
+  vector<vector<char>> traces(const char start_node, const char end_node, const int min_hops,
+                              const int max_hops,
+                              const int max_latency = numeric_limits<int>().max()) const {
     vector<vector<char>> frontier{{start_node}};
     vector<vector<char>> ret{};
     int n_hops = 0;
     while (n_hops < max_hops) {
-      vector<vector<char>> new_frontier{};
+      // for all node lists in the frontier, add all node lists with the new
+      // nodes connected by edges added if they don't have a too high average
+      // latency
+      // check if we should use emplace and move semantics
+      vector<vector<char>> new_frontier;
+
       for (auto nodes : frontier) {
         for (auto dest : NODES) {
           if (graph.find(""s + nodes.back() + dest) == graph.end()) {
@@ -84,82 +104,140 @@ public:
   }
 };
 
-int main(int argc, char **argv) {
-  ::testing::InitGoogleTest(&argc, argv);
-  return RUN_ALL_TESTS();
-}
-
-class GraphTest : public ::testing::Test {
-protected:
-  unique_ptr<Graph> g;
-  GraphTest() {}
-  void SetUp() override {
-    g = make_unique<Graph>(Graph{"AB5,BC4,CD8,DC8,DE6,AD5,CE2,EB3,AE7"});
+string to_string(const optional<int> latency) {
+  stringstream ss;
+  if (latency) {
+    ss << *latency;
+  } else {
+    ss << "NO TRACE FOUND";
   }
-};
-
-// 1. The average latency of the trace A-B-C.
-TEST_F(GraphTest, ex1) {
-  ASSERT_EQ(*g->average_latency(vector<char>{'A', 'B', 'C'}), 9);
+  return ss.str();
 }
 
-// 2. The average latency of the trace A-D.
-TEST_F(GraphTest, ex2) {
-  ASSERT_EQ(*g->average_latency(vector<char>{'A', 'D'}), 5);
-}
+int main() {
+  fstream fs("graph.csv", fstream::in);
+  string graph_str;
+  fs >> graph_str;
+  auto g = make_unique<Graph>(Graph{graph_str});
 
-// 3. The average latency of the trace A-D-C.
-TEST_F(GraphTest, ex3) {
-  ASSERT_EQ(*g->average_latency(vector<char>{'A', 'D', 'C'}), 13);
-}
+  // exercise 1 to 5
+  const array<vector<char>, 5> traces{vector<char>{'A', 'B', 'C'},
+                                      {'A', 'D'},
+                                      {'A', 'D', 'C'},
+                                      {'A', 'E', 'B', 'C', 'D'},
+                                      {'A', 'E', 'D'}};
+  for (const auto &trace : traces) {
+    cout << to_string(g->average_latency(trace)) << endl;
+  }
 
-// 4. The average latency of the trace A-E-B-C-D.
-TEST_F(GraphTest, ex4) {
-  ASSERT_EQ(*g->average_latency(vector<char>{'A', 'E', 'B', 'C', 'D'}), 22);
-}
+  // exercises 6 and 7
+  cout << g->traces('C', 'C', 0, 3).size() << endl;
+  cout << g->traces('A', 'C', 4, 4).size() << endl;
 
-// 5. The average latency of the trace A-E-D.
-TEST_F(GraphTest, ex5) {
-  ASSERT_EQ(g->average_latency(vector<char>{'A', 'E', 'D'}), nullopt);
-}
-// 6. The number of traces originating in service C and ending in service C with
-// a maximum of 3 hops. In the sample data below there are two such traces:
-// C-D-C (2 stops) and C-E-B-C (3 stops).
-TEST_F(GraphTest, ex6) { ASSERT_EQ(g->traces('C', 'C', 0, 3).size(), 2); }
+  // exercises 8 and 9
+  const array<tuple<char, char>, 2> param_tuples{tuple<char, char>{'A', 'C'},
+                                                 {'B', 'B'}};
+  for (const auto &param_tuple : param_tuples) {
+    auto trace_candidates = g->traces(get<0>(param_tuple), get<1>(param_tuple),
+                                      0, Graph::NODES.size());
+    auto shortest_trace_ptr =
+        min_element(trace_candidates.begin(), trace_candidates.end(),
+                    [&](vector<char> a, vector<char> b) {
+                      return *g->average_latency(a) < *g->average_latency(b);
+                    });
+    cout << to_string(g->average_latency(*shortest_trace_ptr)) << endl;
+  }
 
-// 7. The number of traces originating in A and ending in C with exactly 4 hops.
-// In the sample data below there are three such traces: A to C (via B, C, D); A
-// to C (via D, C, D); and A to C (via D, E, B).
-TEST_F(GraphTest, ex7) { ASSERT_EQ(g->traces('A', 'C', 4, 4).size(), 3); }
-// 8. The length of the shortest trace (in terms of latency) between A and C.
-TEST_F(GraphTest, ex8) {
-  auto traces = g->traces('A', 'C', 0, Graph::NODES.size());
-  auto shortest_trace_ptr = min_element(
-      traces.begin(), traces.end(), [&](vector<char> a, vector<char> b) {
-        return *g->average_latency(a) < *g->average_latency(b);
-      });
-  auto min_latency = *g->average_latency(*shortest_trace_ptr);
-  ASSERT_EQ(min_latency, 9);
-}
-// 9. The length of the shortest trace (in terms of latency) between B and B.
-TEST_F(GraphTest, ex9) {
-  auto traces = g->traces('B', 'B', 0, Graph::NODES.size());
-  auto shortest_trace_ptr =
-      min_element(traces.begin(), traces.end(), [&](auto a, auto b) {
-        return *g->average_latency(a) < *g->average_latency(b);
-      });
-  auto min_latency = *g->average_latency(*shortest_trace_ptr);
-  ASSERT_EQ(min_latency, 9);
-}
-//
-// 10. The number of different traces from C to C with an average latency of
-// less than 30. In the same data, the traces are C-D-C, C-E-B-C, C-E-B-C-D-C,
-// C-D-C-E-B-C, C-D-E-B-C, C-E-B-C-E-B-C, C-E-B-C-E-B-C-E-B-C.
-TEST_F(GraphTest, ex10) {
-  auto traces = g->traces('C', 'C', 0, numeric_limits<int>().max(), 29);
-  auto count = count_if(traces.begin(), traces.end(), [&](auto trace) {
+  // exercise 10
+  auto short_traces = g->traces('C', 'C', 0, numeric_limits<int>().max(), 29);
+  cout << count_if(short_traces.begin(), short_traces.end(), [&](auto trace) {
     return *g->average_latency(trace) < 30;
-  });
-  ASSERT_EQ(count, 7);
+  }) << endl;
+
+  /* ::testing::InitGoogleTest(&argc, argv); */
+  /* return RUN_ALL_TESTS(); */
 }
+
+/* class GraphTest : public ::testing::Test { */
+/* protected: */
+/*   unique_ptr<Graph> g; */
+/*   GraphTest() {} */
+/*   void SetUp() override { */
+/*     g = make_unique<Graph>(Graph{"AB5,BC4,CD8,DC8,DE6,AD5,CE2,EB3,AE7"}); */
+/*   } */
+/* }; */
+
+/* // 1. The average latency of the trace A-B-C. */
+/* TEST_F(GraphTest, ex1) { */
+/*   ASSERT_EQ(*g->average_latency(vector<char>{'A', 'B', 'C'}), 9); */
 /* } */
+
+/* // 2. The average latency of the trace A-D. */
+/* TEST_F(GraphTest, ex2) { */
+/*   ASSERT_EQ(*g->average_latency(vector<char>{'A', 'D'}), 5); */
+/* } */
+
+/* // 3. The average latency of the trace A-D-C. */
+/* TEST_F(GraphTest, ex3) { */
+/*   ASSERT_EQ(*g->average_latency(vector<char>{'A', 'D', 'C'}), 13); */
+/* } */
+
+/* // 4. The average latency of the trace A-E-B-C-D. */
+/* TEST_F(GraphTest, ex4) { */
+/*   ASSERT_EQ(*g->average_latency(vector<char>{'A', 'E', 'B', 'C', 'D'}), 22);
+ */
+/* } */
+
+/* // 5. The average latency of the trace A-E-D. */
+/* TEST_F(GraphTest, ex5) { */
+/*   ASSERT_EQ(g->average_latency(vector<char>{'A', 'E', 'D'}), nullopt); */
+/* } */
+/* // 6. The number of traces originating in service C and ending in service C
+ * with */
+/* // a maximum of 3 hops. In the sample data below there are two such traces:
+ */
+/* // C-D-C (2 stops) and C-E-B-C (3 stops). */
+/* TEST_F(GraphTest, ex6) { ASSERT_EQ(g->traces('C', 'C', 0, 3).size(), 2); } */
+
+/* // 7. The number of traces originating in A and ending in C with exactly 4
+ * hops. */
+/* // In the sample data below there are three such traces: A to C (via B, C,
+ * D); A */
+/* // to C (via D, C, D); and A to C (via D, E, B). */
+/* TEST_F(GraphTest, ex7) { ASSERT_EQ(g->traces('A', 'C', 4, 4).size(), 3); } */
+/* // 8. The length of the shortest trace (in terms of latency) between A and C.
+ */
+/* TEST_F(GraphTest, ex8) { */
+/*   auto traces = g->traces('A', 'C', 0, Graph::NODES.size()); */
+/*   auto shortest_trace_ptr = min_element( */
+/*       traces.begin(), traces.end(), [&](vector<char> a, vector<char> b) { */
+/*         return *g->average_latency(a) < *g->average_latency(b); */
+/*       }); */
+/*   auto min_latency = *g->average_latency(*shortest_trace_ptr); */
+/*   ASSERT_EQ(min_latency, 9); */
+/* } */
+/* // 9. The length of the shortest trace (in terms of latency) between B and B.
+ */
+/* TEST_F(GraphTest, ex9) { */
+/*   auto traces = g->traces('B', 'B', 0, Graph::NODES.size()); */
+/*   auto shortest_trace_ptr = */
+/*       min_element(traces.begin(), traces.end(), [&](auto a, auto b) { */
+/*         return *g->average_latency(a) < *g->average_latency(b); */
+/*       }); */
+/*   auto min_latency = *g->average_latency(*shortest_trace_ptr); */
+/*   ASSERT_EQ(min_latency, 9); */
+/* } */
+/* // */
+/* // 10. The number of different traces from C to C with an average latency of
+ */
+/* // less than 30. In the same data, the traces are C-D-C, C-E-B-C,
+ * C-E-B-C-D-C, */
+/* // C-D-C-E-B-C, C-D-E-B-C, C-E-B-C-E-B-C, C-E-B-C-E-B-C-E-B-C. */
+/* TEST_F(GraphTest, ex10) { */
+/*   auto traces = g->traces('C', 'C', 0, numeric_limits<int>().max(), 29); */
+/*   auto count = count_if(traces.begin(), traces.end(), [&](auto trace) { */
+/*     return *g->average_latency(trace) < 30; */
+/*   }); */
+/*   ASSERT_EQ(count, 7); */
+/* } */
+/* /1* } *1/ */
